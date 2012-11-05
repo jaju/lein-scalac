df601d46e2b19828a9c29f8c0423dbebfcfe8fce
Jens Haase <je.haase@googlemail.com>
2012-11-05 12:32:26 +0100

    configuration of different scala versions

--- src/leiningen/scalac.clj
+++ src/leiningen/scalac.clj
@@ -1,5 +1,6 @@
 (ns leiningen.scalac
   (:require [lancet.core :as lancet]
+            [leiningen.core.eval :as eval]
             [leiningen.classpath :as classpath])
   (:import (org.apache.tools.ant.types Path)))
 
@@ -8,19 +9,27 @@
           :destdir (:compile-path project)}
          (:scalac-options project)))
 
-(.addTaskDefinition lancet/ant-project "scalac" scala.tools.ant.Scalac)
-
-(lancet/define-ant-task ant-scalac scalac)
-
 (defn scalac
   "Compile Scala source in :scala-source-path to :compile-path.
 
 Set :scalac-options in project.clj to pass options to the Scala compiler.
 See http://www.scala-lang.org/node/98 for details."
   [project]
-  (let [classpath (classpath/get-classpath-string project)
-        task (doto (lancet/instantiate-task lancet/ant-project "scalac"
-                                            (task-props project))
-               (.setClasspath (Path. lancet/ant-project classpath)))]
-    (lancet/mkdir {:dir (:compile-path project)})
-    (.execute task)))
+  (eval/eval-in-project
+   (update-in
+    project [:dependencies]
+    conj
+    ['org.scala-lang/scala-compiler (or (:scala-version project) "2.9.2")]
+    ['lancet "1.0.1"])
+   `(do
+      (require 'lancet.core)
+      (.addTaskDefinition
+       lancet.core/ant-project "scalac" scala.tools.ant.Scalac)
+      (lancet.core/define-ant-task ~(symbol 'ant-scalac) ~(symbol 'scalac))
+      (let [classpath# ~(classpath/get-classpath-string project)
+            task# (doto (lancet.core/instantiate-task
+                         lancet.core/ant-project "scalac"
+                         ~(task-props project))
+                    (.setClasspath (Path. lancet.core/ant-project classpath#)))]
+        (lancet.core/mkdir {:dir ~(:compile-path project)})
+        (.execute task#)))))
