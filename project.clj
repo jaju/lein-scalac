39a2a0e4bf8443a201b3c15cab9e03dfb1368ea1
Ravindra R. Jaju <jaju@msync.org>
2013-03-16 08:55:46 +0530

    Namespaced the project to allow for publishing on clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scalac "0.2.0-SNAPSHOT"
+(defproject org.msync/lein-scalac "0.2.0-SNAPSHOT"
   :eval-in-leiningen true
   :dependencies [[org.scala-lang/scala-compiler "2.9.1" :scope "provided"]
                  [lancet "1.0.1"]])
